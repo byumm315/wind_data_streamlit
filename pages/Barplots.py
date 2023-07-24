f9caedc934be5b219419dafbe67f290cc11b852b
박유미 <81951313+byumm315@users.noreply.github.com>
2023-07-25 01:55:56 +0900

    Update Barplots.py

--- pages/Barplots.py
+++ pages/Barplots.py
@@ -18,5 +18,5 @@ v2_list = ['섭씨온도','절대온도','이슬점','상대습도','대기압',
               '수증기함량','공기밀도','풍향','풍속']
 vari2 = st.selectbox(label = "Choose a Variable", options = v2_list,key=1)
 title = f"The Bar plot of Month, Time Zone and {vari2}"
-fig = px.bar(data, x = '월', y = vari2,hue='시간대',title=title)
+fig = px.bar(data, x = '월', y = vari2,color="시간대", barmode = 'group',title=title)
 st.plotly_chart(fig)
